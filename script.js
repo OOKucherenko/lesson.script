c0156905eaf430d490753d6abac273c25b59e4d3
OOKucherenko <okld52dec@gmail.com>
2023-02-18 11:51:59 +0200

    task1

---
+++ script.js
@@ -0,0 +1,55 @@
+// Example 1 - Математичні оператори
+// Виведи на екран загальну кількість яблук та винограду. Різницю яблук та винограду.
+
+// const apples = 47;
+// const grapes = 135;
+// const total = apples + grapes;
+// console.log(total);
+// const diff = apples - grapes;
+// console.log(diff);
+
+//Example 2 - Комбіновані оператори
+//Заміни вираз перевизначення комбінованим оператором +=.
+
+// let students = 100;
+// students += 50;
+// console.log(students);
+
+// Example 3 - Пріоритет операторів
+// Розбери пріоритет операторів в інструкції привласнення значення змінної result.
+
+// const result = 108 + 223 - 2 * 5;
+// console.log(result);
+
+// Example 4 - Клас Math
+// Напиши скрипт, який виводить у консоль заокруглені вгору/вниз і т.д. значення змінної value.
+// Використовуй методи Math.floor(), Math.ceil() та Math.round().Перевір що буде в консолі при значеннях 27.3 та 27.9.
+
+// const value = 27.3;
+// console.log(Math.floor(value));
+// console.log(Math.ceil(value));
+// console.log(Math.round(value));
+
+// Example 5 - Шаблонні рядки
+// Склади фразу за допомогою шаблонних рядків A has B bots in stock, де A, B - змінні вставлені в рядок.
+
+// const companyName = "Cyberdyne Systems";
+// const repairBots = 150;
+// const defenceBots = 50;
+// const message = `${"Cyberdyne Systems"} has ${
+//   repairBots + defenceBots
+// } bots in stock`;
+// console.log(message); // "Cyberdyne Systems has 200 bots in stock"
+
+// Example 6 - Методи рядків та чейнінг
+// Напиши скрипт, який розраховує індекс маси тіла людини. Для цього необхідно розділити вагу в кілограмах на квадрат висоти людини у метрах.
+
+// Вага та висота зберігаються у змінних weight та height, але не як числа, а в вигляді рядків (спеціально для завдання). Не цілі числа можуть бути задані у вигляді 24.7 або 24,7, тобто як роздільник дробової частини може бути кома.
+
+// Індекс маси тіла необхідно округлити до однієї цифри після коми;
+
+let weight = '88,3';
+let height = '1.75';
+
+const bmi = ;
+console.log(bmi); // 28.8
